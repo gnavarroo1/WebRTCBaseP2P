4d6edb62f1311dc23421c78c2a4481becf9059c5
Gustavo Navarro <g.navarroo@hotmail.com>
2021-04-21 12:13:49 -0500

    test

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import { resolve } from 'node:path';
 import * as path from 'path';
 import {
     v4 as uuidV4, v4
@@ -6,7 +7,7 @@ import {
 
 export class Routes {
     private app: express.Application;
-    private rooms : any[];
+    private rooms : any;
     constructor(app: express.Application) {
         this.app = app;
         this.rooms = [];
@@ -16,29 +17,58 @@ export class Routes {
     
         this.app.get('/', (request, response) => {
             const id = v4();
-            console.log(id)
-            response.render('pages/index', {uuid: id})
+            if(!request.cookies.userId)
+                response.cookie("userId",id,{ maxAge: 3600000 })
+            // console.log(request.cookies);
+            response.render('pages/index')
         });
         
-        this.app.post('/room', (request, response) => {            
-            console.log(request.body)
-            response.render('pages/room')
+        this.app.post('/room', (request, response) => {        
+            const id = v4();
             
+            this.rooms.push({
+                'count' : this.rooms.length + 1,
+                'roomId' : id,
+                'userCreatorId' : id,
+                'users' : [
+                    id
+                ],
+                'roomName' : request.body.txt_room
+            })
+            response.redirect('/room/'+id);
         });
 
         this.app.get('/room/:uuid', (request, response) => {            
-            const uuid = request.params.uuid
-            var room = 
-            response.render('pages/room', {uuid: uuid})
+            const uuid = request.params.uuid;
+            const userId = request.cookies.userId;
+            if(!userId){
+                console.log("error")
+            }
+            let name  = uuid;
+            if(this.rooms){
+                // console.log(uuid)
+                let __room = this.rooms.find((room,index) => {
+                    // console.log(room);
+                    return room.roomId === uuid
+                })
+                if(__room){
+                    name = __room.roomName || 'SALA #' + __room.count
+                    __room.roomName = name;
+                }else{
+                    //TODO Manejo de error para el ingreso directo una sala x url sin que esta haya sido creada / registrada
+                    console.log('ERROR')
+                }
+            }
+            // console.log(this.rooms);
+            
+            response.render('pages/room', {uuid: uuid , name: name})
         });
 
-    
     }
-
 
     public getRoutes(): void {
         this.home();
-        
     }
+    
 
 }
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,18 +1,20 @@
-import express = require('express');
+import express, { Request, Response } from 'express';
 import { createServer, Server } from 'http';
 import * as socketIo from 'socket.io';
+// import ioclient from 'socket.io-client';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import helmet = require("helmet");
 import * as path from 'path';
+import cookieParser from 'cookie-parser';
 import { Socket } from 'socket.io';
-
 export class App{
-    public static readonly PORT = process.env.PORT;
+    public static readonly PORT = process.env.PORT || 5000;
     private app: express.Application;
     private port: string | number;
     private server: Server;
     private io: socketIo.Server;
+    private rooms = {};
     constructor(){
         this.createApp();
         this.config();
@@ -21,7 +23,6 @@ export class App{
         this.setJSLibrariesFiles();
         this.sockets();
         this.listen();
-
     }
 
 
@@ -33,7 +34,8 @@ export class App{
         
         this.app.use(express.static(path.join(__dirname,'../public')));
         this.app.set('views', path.join(__dirname, '../views'));
-        this.app.set('view engine','ejs');     
+        this.app.set('view engine','ejs');    
+        this.app.use(cookieParser()) ;
         this.app.use(cors());
         this.app.use(helmet({
             contentSecurityPolicy: false,
@@ -49,6 +51,7 @@ export class App{
     private setJSLibrariesFiles():void{
         this.app.use('/libraries/bootstrap/js',express.static(path.join(__dirname, '../../node_modules/bootstrap/dist/js')));
         this.app.use('/libraries/jquery/js',express.static(path.join(__dirname, '../../node_modules/jquery/dist')));
+        this.app.use('/libraries/socket.io-client',express.static(path.join(__dirname, '../../node_modules/socket.io-client/dist')));
     }
 
     private config(): void {
@@ -63,23 +66,71 @@ export class App{
         return this.app;
     }
 
-    
+    public getServer(): Server{
+        return this.server;
+    }   
     private listen(): void {
         this.server.listen(this.port, () => {
             console.log('Running server on port %s', this.port);
         });
 
-        this.io.on("connection",(socket: Socket) =>{
-            socket.broadcast.emit('add-users', {
-                users: [socket.id]
-               });
-        })
+        
     }
-
 
     private sockets():void{
-        this.io = new socketIo.Server(this.server);
+        this.io = new socketIo.Server(this.server); 
+        this.io.on("connection",socket =>{
+            this.socketNotificationEvents(socket);
+            this.signalingHandshakeEvents(socket);
+        })  
     }
 
+    private socketNotificationEvents(socket:any){
+        
+        socket.on("join-room", (roomId) =>{
+            console.log("-----------------");
+            console.log(roomId)
+            console.log("-----------------");
+            //Si la sala existe se añade el id del socket a la sala
+            if(this.rooms[roomId]){
+                this.rooms[roomId].push(socket.id);
+            }else{ // sino se agrega el id de la sala a la lista como key y el valor un array con el id del socket
+                this.rooms[roomId] = [socket.id];
+            }
+            socket.join(roomId);
+            const user = this.rooms[roomId].find(id => id !== socket.id)
+            socket.to(roomId).emit('join-room',socket.id)
+            // if(user){
+            //     socket.emit('other user', user);
+            //     socket.to(roomId).broadcast.emit('user joined',socket.id);
+            // }
+            //socket.join(roomId);
+            //socket.to(roomId).broadcast.emit("user-connected",userId);
+        });
+        socket.on("video-pause", () =>{
+
+        });
+        socket.on("audio-mute", () =>{
+
+        });
+        socket.on("disconnect", () =>{
+
+        });
+    }
+
+
+    private signalingHandshakeEvents(socket: Socket){
+        socket.on("signaling-offer", payload =>{
+            this.io.to(payload.target).emit('signaling-offer',payload)
+        });
+
+        socket.on("signaling-answer", payload =>{
+            this.io.to(payload.target).emit('signaling-answer',payload)
+        });
+
+        socket.on('ice-candidate', incoming => {
+            this.io.to(incoming.target).emit('ice-candidate', incoming.candidate);
+          });
+    }
     
 }
